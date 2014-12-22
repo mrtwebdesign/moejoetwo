1892fbc38caf5ce5ffdfe3045fe931bbb097a6b9
Matthew Taylor <matthew@mrtwebdesign.com>
2014-12-21 21:00:40 -0800

    Adding CMB2 to theme for events

--- footer.php
+++ footer.php
@@ -30,7 +30,6 @@
 
 	<div class="site-info">
 		<?php do_action( 'twentytwelve_credits' ); ?>
-		<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
